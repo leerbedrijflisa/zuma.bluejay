0f342e8b362d8cfbfa3a15656fbc9289ae8a66e3
Maikel de Hoog <maikel@pixxa.nl>
2014-02-25 11:58:52 +0100

    Refactored some code and added the login setter

--- src/App/Lisa.Zuma.BlueJay.IOS.Data/Database.cs
+++ src/App/Lisa.Zuma.BlueJay.IOS.Data/Database.cs
@@ -6,20 +6,30 @@ namespace Lisa.Zuma.BlueJay.IOS.Data
 	public class Database
 	{
 		private string pathToDatabase;
+		private SQLite.SQLiteConnection db;
 
 		public Database ()
 		{
-			// Figure out where the SQLite database will be.
 			var documents = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
 			pathToDatabase = Path.Combine(documents, "BlueJay_DB.db");
 
-			using (var db = new SQLite.SQLiteConnection(pathToDatabase))
-			{
-				db.CreateTable<Dosier>();
-				db.CreateTable<Notes>();
-				db.CreateTable<Profile>();
-				db.CreateTable<User>();
-			}
+			db = new SQLite.SQLiteConnection (pathToDatabase);
+
+			db.CreateTable<Dosier>();
+			db.CreateTable<Notes>();
+			db.CreateTable<Profile>();
+			db.CreateTable<User>();
+
+			User parent = new User { Role = "Parent", Name = "Debbie"};
+			User superVisor = new User { Role = "Supervisor", Name = "Marie-Antoinette"};
+
+			db.Insert (superVisor);
+			db.Insert(parent);
+		}
+
+		public void DummyLoggedIn(int id)
+		{
+			db.Query<User> ("UPDATE UPDATE User SET LoggedIn = 1 WHERE ID = '"+ id +"'");
 		}
 	}
 }
