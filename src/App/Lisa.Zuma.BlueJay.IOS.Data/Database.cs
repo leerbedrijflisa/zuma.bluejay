bb8cf5c2dc179bb4c6c417dac56629b1b325b59c
Maikel de Hoog <maikel@pixxa.nl>
2014-02-25 15:27:48 +0100

    Filled list with data

--- src/App/Lisa.Zuma.BlueJay.IOS.Data/Database.cs
+++ src/App/Lisa.Zuma.BlueJay.IOS.Data/Database.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Lisa.Zuma.BlueJay.IOS.Data
 {
@@ -21,12 +22,21 @@ namespace Lisa.Zuma.BlueJay.IOS.Data
 			db.CreateTable<Profile>();
 			db.CreateTable<User>();
 
+			db.Insert (new Dosier { Name = "Martijn Olga" });
+
 		}
 
 		public void DummyLoggedIn(int id)
 		{
 			db.Query<User>("UPDATE User SET LoggedIn = 0");
 			db.Query<User>("UPDATE User SET LoggedIn = 1 WHERE ID = '"+ id +"'");
+		}
+
+		public List<Dosier> GetDosiers(int id)
+		{
+			var Result = db.Query<Dosier>("SELECT * FROM Dosier WHERE ID='"+id+"'");
+
+			return Result;
 		}
 	}
 }
--- src/App/Lisa.Zuma.BlueJay.IOS.Views/SummaryViewController.cs
+++ src/App/Lisa.Zuma.BlueJay.IOS.Views/SummaryViewController.cs
@@ -2,27 +2,36 @@ using System;
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using Lisa.Zuma.BlueJay.IOS.Models;
 
 namespace Lisa.Zuma.BlueJay.IOS
 {
 	public partial class SummaryViewController : UIViewController
 	{
+		private TableHelper tableHelper;
+
 		public SummaryViewController () : base ("SummaryViewController", null)
 		{
+			tableHelper = new TableHelper ();
 		}
 
 		public override void DidReceiveMemoryWarning ()
 		{
-			// Releases the view if it doesn't have a superview.
 			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			this.NavigationItem.SetHidesBackButton (true, false);
+
+			tblCell.Source = tableHelper.DataForList();
+
+		}
+
+		public void tableSource_RowClicked (object sender, RowClickedEventArgs e)
+		{
+			Console.WriteLine (e.ClickedItem.Id);
 		}
 	}
 }
