236b55af5a47a2ee2a018faec643efaacc089770
Maikel de Hoog <maikel@pixxa.nl>
2014-02-25 10:37:43 +0100

    Initial commit

---
+++ src/App/Lisa.Zuma.BlueJay.IOS.Data/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lisa.Zuma.BlueJay.IOS.Data
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ src/App/Lisa.Zuma.BlueJay.IOS.Models/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lisa.Zuma.BlueJay.IOS.Models
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
