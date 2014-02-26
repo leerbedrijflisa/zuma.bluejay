1d3842be5131d9d8ec40f0bc7ef9b962a785f1eb
Robert <azforeversupporter@gmail.com>
2014-02-26 11:13:35 +0100

    Added models returned by the webapi actions.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web.Models/DbModels/DossierModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lisa.Zuma.BlueJay.Web.Models.DbModels
+{
+    public class DossierModel
+    {
+        public int Id { get; set; }
+        public List<NoteModel> Notes { get; set; }
+    }
+}
---
+++ src/Web/Lisa.Zuma.BlueJay.Web.Models/DbModels/NoteModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lisa.Zuma.BlueJay.Web.Models.DbModels
+{
+    public class NoteModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public List<NoteMediaModel> Media { get; set; }
+    }
+}
