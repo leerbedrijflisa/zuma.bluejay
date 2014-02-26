976396e99922628c302f7f25a70531df7d2d4af5
Robert <azforeversupporter@gmail.com>
2014-02-26 12:04:01 +0100

    Added ModelFactory for creating models to return from api actions.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web.Models/ModelFactory.cs
@@ -0,0 +1,49 @@
+﻿using Lisa.Zuma.BlueJay.Web.Data.Entities;
+using Lisa.Zuma.BlueJay.Web.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lisa.Zuma.BlueJay.Web.Models
+{
+    public static class ModelFactory
+    {
+        public static DossierModel Create(Dossier dossier)
+        {
+            var model = new DossierModel
+            {
+                Id = dossier.Id,
+                Notes = new List<NoteModel>()
+            };
+
+            model.Profile = new ProfileModel
+            {
+                Id = dossier.Profile.Id
+            };
+
+            foreach (var note in dossier.Notes)
+            {
+                var noteModel = new NoteModel
+                {
+                    Id = note.Id,
+                    Media = new List<NoteMediaModel>(),
+                    Text = note.Text
+                };
+
+                foreach (var media in note.Media)
+                {
+                    var noteMediaModel = new NoteMediaModel
+                    {
+                        Name = media.Name
+                    };
+
+                    noteModel.Media.Add(noteMediaModel);
+                }
+
+                model.Notes.Add(noteModel);
+            }
+
+            return model;
+        }
+    }
+}
