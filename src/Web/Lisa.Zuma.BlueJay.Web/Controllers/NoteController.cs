0f4ee46a36292b289cea44f56d24e0b05c4f4dbb
Robert <azforeversupporter@gmail.com>
2014-02-26 16:19:07 +0100

    Added NoteController, responsible for all note related operations. Still needs more CRUD operations implemented.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web/Controllers/NoteController.cs
@@ -0,0 +1,82 @@
+﻿using Lisa.Zuma.BlueJay.Web.Data.Entities;
+using Lisa.Zuma.BlueJay.Web.Models;
+using Lisa.Zuma.BlueJay.Web.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lisa.Zuma.BlueJay.Web.Controllers
+{
+    public class NoteController : BaseApiController
+    {
+        /// <summary>
+        /// Gets a list of notes belonging to the requested dossier.
+        /// </summary>
+        /// <param name="id">The id of the dossier from which to retrieve the notes.</param>
+        public IHttpActionResult Get(int id)
+        {
+            var dossier = Db.Dossiers.FirstOrDefault(d => d.Id == id);
+            if (dossier == null)
+            {
+                return NotFound();
+            }
+
+            var result = new List<NoteModel>();
+
+            foreach (var note in dossier.Notes)
+            {
+                var model = ModelFactory.Create(note);
+                result.Add(model);
+            }
+
+            return Ok(result);
+        }
+
+        public IHttpActionResult Post(int id, [FromBody] NoteModel noteModel)
+        {
+            var dossier = Db.Dossiers.FirstOrDefault(d => d.Id == id);
+            if (dossier == null)
+            {
+                return NotFound();
+            }
+
+            var note = new Note
+            {
+                Text = noteModel.Text,
+                Media = new List<NoteMedia>()
+            };
+
+            foreach (var media in noteModel.Media)
+            {
+                var noteMedia = StoreMedia(media);
+                note.Media.Add(noteMedia);
+            }
+
+            dossier.Notes.Add(note);
+            Db.SaveChanges();
+
+            return CreatedAtRoute("NoteApi", new { id = id }, noteModel);
+        }
+
+        public NoteMedia StoreMedia(NoteMediaModel media)
+        {
+            var result = new NoteMedia();
+
+#if DEBUG
+            var path = @"D:\zuma_storage\" + media.Name;
+            var bytes = Convert.FromBase64String(media.EncodedData);
+            System.IO.File.WriteAllBytes(path, bytes);
+
+            result.Name = media.Name;
+            result.MediaLocation = path;
+#else
+            // TODO: Add support for the cloud
+#endif
+
+            return result;
+        }
+    }
+}
