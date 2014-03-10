c236fb67b99ddb592e1572a01ab7204821686b63
Robert <azforeversupporter@gmail.com>
2014-03-10 13:18:39 +0100

    Added DossierDetail entity class and DossierDetailModel class to replace the Profile class.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web.Data/Entities/DossierDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lisa.Zuma.BlueJay.Web.Data.Entities
+{
+    public class DossierDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Contents { get; set; }
+
+        [Required]
+        public int DossierId { get; set; }
+        public virtual Dossier Dossier { get; set; }
+    }
+}
