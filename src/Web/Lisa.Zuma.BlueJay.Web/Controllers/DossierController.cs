8ec15b2414df0c2dd5089c457e514b2bd33f5848
Robert <azforeversupporter@gmail.com>
2014-03-26 20:30:56 +0100

    Added Authorize attribute and load dossier available to the user.

--- src/Web/Lisa.Zuma.BlueJay.Web/Controllers/DossierController.cs
+++ src/Web/Lisa.Zuma.BlueJay.Web/Controllers/DossierController.cs
@@ -2,6 +2,7 @@
 using Lisa.Zuma.BlueJay.Web.Data.Entities;
 using Lisa.Zuma.BlueJay.Web.Helpers;
 using Lisa.Zuma.BlueJay.Web.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +12,19 @@ using System.Web.Http;
 
 namespace Lisa.Zuma.BlueJay.Web.Controllers
 {
+    [Authorize]
     public class DossierController : BaseApiController
     {
         public IHttpActionResult Get(int id)
         {
-            var user = Db.Users.FirstOrDefault(u => u.Id == id);
+            string userId = User.Identity.GetUserId();
+            var user = Db.Users.Find(userId);
             if (user == null)
             {
                 return NotFound();
             }
 
-            var dossier = Db.Dossiers.FirstOrDefault(d => d.Watchers.Count(u => u.Id == user.Id) > 0);
+            var dossier = user.Dossiers.FirstOrDefault(d => d.Id == id);
             if (dossier == null)
             {
                 return NotFound();
@@ -30,6 +33,25 @@ namespace Lisa.Zuma.BlueJay.Web.Controllers
             var dossierModel = Converter.ToDossier(dossier);
 
             return Ok(dossierModel);
+        }
+
+        public IHttpActionResult Get()
+        {
+            string userId = User.Identity.GetUserId();
+            var user = Db.Users.Find(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var dossiers = Db
+                .Dossiers
+                .Where(d => d.Watchers.Count(u => u.Id == user.Id) > 0)
+                .ToList();
+
+            var dossierModels = Converter.ToDossier(dossiers);
+
+            return Ok(dossierModels);
         }
     }
 }
