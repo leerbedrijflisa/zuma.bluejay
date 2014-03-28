b63e045ff1eb1394d6497593a78bf03d64f61730
Robert <azforeversupporter@gmail.com>
2014-03-28 12:37:02 +0100

    Added documentation for the UserManagerExtensions methods.

--- src/Web/Lisa.Zuma.BlueJay.Web/Extensions/UserManagerExtensions.cs
+++ src/Web/Lisa.Zuma.BlueJay.Web/Extensions/UserManagerExtensions.cs
@@ -10,12 +10,20 @@ namespace Lisa.Zuma.BlueJay.Web.Extensions
 {
     public static class UserManagerExtensions
     {
+        /// <summary>
+        /// Gets the currently logged-in user.
+        /// </summary>
+        /// <typeparam name="TUser">A class that inherits from the IdentityUser class.</typeparam>
         public static TUser GetCurrentUser<TUser>(this UserManager<TUser> userManager) where TUser : IdentityUser
         {
             var userId = GetCurrentUserId();
             return userManager.FindById(userId);
         }
 
+        /// <summary>
+        /// Gets the user id of the currently logged-in user.
+        /// </summary>
+        /// <returns></returns>
         public static string GetCurrentUserId()
         {
             return HttpContext.Current.User.Identity.GetUserId();
