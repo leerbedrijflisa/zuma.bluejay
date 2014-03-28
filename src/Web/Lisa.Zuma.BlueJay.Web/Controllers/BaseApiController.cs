5eb671b8f60cf195be7a8b8ce8cbb81190e0c1c7
Robert <azforeversupporter@gmail.com>
2014-03-28 12:22:11 +0100

    Replaced method GetCurrentUser with the property CurrentUser. Added documentation for the property CurrentUser.

--- src/Web/Lisa.Zuma.BlueJay.Web/Controllers/BaseApiController.cs
+++ src/Web/Lisa.Zuma.BlueJay.Web/Controllers/BaseApiController.cs
@@ -16,9 +16,17 @@ namespace Lisa.Zuma.BlueJay.Web.Controllers
         protected BlueJayContext Db { get; private set; }
         protected UserManager<UserData> UserManager { get; private set; }
 
-        protected UserData GetCurrentUser()
+        /// <summary>
+        /// Gets the currently logged-in user for this request.
+        /// When used in a controller action which does not require authorization,
+        /// a null check must be used before using this variable.
+        /// </summary>
+        protected UserData CurrentUser
         {
-            return UserManager.GetCurrentUser();
+            get
+            {
+                return UserManager.GetCurrentUser();
+            }
         }
 
         protected override void Initialize(HttpControllerContext controllerContext)
