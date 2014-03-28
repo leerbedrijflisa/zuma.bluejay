8f5d8b7011aacf38b744c533a755f9b4567966e7
Maikel de Hoog <maikel@pixxa.nl>
2014-03-28 12:22:40 +0100

    Fixed the indicators

--- src/App/Lisa.Zuma.BlueJay.IOS.Views/LockScreenViewController.cs
+++ src/App/Lisa.Zuma.BlueJay.IOS.Views/LockScreenViewController.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using System.Collections.Generic;
 
 namespace Lisa.Zuma.BlueJay.IOS.Views
 {
@@ -9,36 +10,83 @@ namespace Lisa.Zuma.BlueJay.IOS.Views
 	{
 		public LockScreenViewController () : base ("LockScreenViewController", null)
 		{
+			DummyCombination = 1234;
 		}
 			
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-		
+			ButtonHandle ();
 		}
 
-		private void InitializeUI()
+		private void ButtonHandle()
 		{
-			btnNumberOne.TouchUpInside += WriteToConsole;
-			btnNumberTwo.TouchUpInside += WriteToConsole;
-			btnNumberThree.TouchUpInside +=WriteToConsole;
-			btnNumberFour.TouchUpInside += WriteToConsole;
-			btnNumberFive.TouchUpInside += WriteToConsole;
-			btnNumberSix.TouchUpInside += WriteToConsole;
-			btnNumberSeven.TouchUpInside += WriteToConsole;
-			btnNumberEight.TouchUpInside += WriteToConsole;
-			btnNumberNine.TouchUpInside += WriteToConsole;
-			btnNumberZero.TouchUpInside += WriteToConsole;
+			btnNumberOne.TouchUpInside += setNumberToList;
+			btnNumberTwo.TouchUpInside += setNumberToList;
+			btnNumberThree.TouchUpInside +=setNumberToList;
+			btnNumberFour.TouchUpInside += setNumberToList;
+			btnNumberFive.TouchUpInside += setNumberToList;
+			btnNumberSix.TouchUpInside += setNumberToList;
+			btnNumberSeven.TouchUpInside += setNumberToList;
+			btnNumberEight.TouchUpInside += setNumberToList;
+			btnNumberNine.TouchUpInside += setNumberToList;
+			btnNumberZero.TouchUpInside += setNumberToList;
 		}
 
-		public void WriteToConsole(Object sender, EventArgs args)
+		private void setNumberToList(Object sender, EventArgs args)
 		{
 			var txt = sender as UIButton;
+
 			if (txt != null) {
-				var val = txt.CurrentTitle;
-				Console.WriteLine (val);
+
+				Combination += txt.CurrentTitle;
+				FillCombinationButton ();
+
+				Console.WriteLine (Combination.Length);
+
+				if (Combination.Length == 4) {
+//					NSThread.SleepFor (200);
+					if (Combination == DummyCombination.ToString ()) {
+						Console.WriteLine ("Good Job !");
+						clearChosenCombination ();
+					} else {
+						Console.WriteLine ("Nope !");
+						clearChosenCombination ();
+					}
+				}
+
 			}
 		}
+
+		private void clearChosenCombination(){
+			imvInputOne.Image = UIImage.FromBundle ("number-emtpy.png");
+			imvInputTwo.Image = UIImage.FromBundle ("number-emtpy.png");
+			imvInputThree.Image = UIImage.FromBundle ("number-emtpy.png");
+			imvInputFour.Image = UIImage.FromBundle ("number-emtpy.png");
+			Combination = string.Empty;
+		}
+
+		private void FillCombinationButton(){
+			var value = Combination.Length;
+
+			switch (value) {
+			case 1:
+				imvInputOne.Image = UIImage.FromBundle ("number-insert.png");
+				break;
+			case 2: 
+				imvInputTwo.Image = UIImage.FromBundle ("number-insert.png");
+				break;
+			case 3:
+				imvInputThree.Image = UIImage.FromBundle ("number-insert.png");
+				break;
+			case 4:
+				imvInputFour.Image = UIImage.FromBundle ("number-insert.png");
+				break;
+			}
+		}
+
+		private string Combination;
+		private int DummyCombination;
 	}
 }
 
