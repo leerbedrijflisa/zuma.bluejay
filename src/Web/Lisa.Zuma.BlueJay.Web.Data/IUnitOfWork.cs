e33071b24e19f449c2771035c02056af71c91e62
Robert <azforeversupporter@gmail.com>
2014-04-02 22:22:15 +0200

    Added interface for the GenericRepository, and added an interface for the UnitOfWork. All to make future extensions or additional implementation possible.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web.Data/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Lisa.Zuma.BlueJay.Web.Data.Entities;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace Lisa.Zuma.BlueJay.Web.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<DossierData> DossierRepository { get; }
+        IGenericRepository<DossierDetailData> DossierDetailRepository { get; }
+        IGenericRepository<NoteData> NoteRepository { get; }
+        IGenericRepository<NoteMediaData> NoteMediaRepository { get; }
+        UserManager<UserData> UserManager { get; }
+
+        void Save();
+    }
+}
