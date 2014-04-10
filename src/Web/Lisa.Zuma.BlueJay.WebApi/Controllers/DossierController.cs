95e802d730df4f14c77ac51bd02676c1709629e4
Robert <azforeversupporter@gmail.com>
2014-04-10 15:15:27 +0200

    Moved Lisa.Zuma.BlueJay.Web to Lisa.Zuma.BlueJay.WebApi. Changed default namespace and refactored current namespaces to reflect the namespace change.

---
+++ src/Web/Lisa.Zuma.BlueJay.WebApi/Controllers/DossierController.cs
@@ -0,0 +1,38 @@
+﻿using Lisa.Zuma.BlueJay.Web.Data;
+using Lisa.Zuma.BlueJay.Web.Data.Entities;
+using Lisa.Zuma.BlueJay.Web.Data.Extensions;
+using Lisa.Zuma.BlueJay.WebApi.Helpers;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lisa.Zuma.BlueJay.WebApi.Controllers
+{
+    [Authorize]
+    public class DossierController : BaseApiController
+    {
+        public IHttpActionResult Get(int id)
+        {
+            var dossier = default(DossierData);
+            if (!CurrentUser.TryGetDossier(id, out dossier))
+            {
+                return NotFound();
+            }
+                        
+            var dossierModel = Converter.ToDossier(dossier);
+
+            return Ok(dossierModel);
+        }
+
+        public IHttpActionResult Get()
+        {
+            var dossierModels = Converter.ToDossier(CurrentUser.Dossiers);
+
+            return Ok(dossierModels);
+        }
+    }
+}
