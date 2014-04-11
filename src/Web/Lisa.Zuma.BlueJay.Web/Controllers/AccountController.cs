8f73148fdff8c523ce7ea5e9f7279d3ea7cb5d58
Robert <azforeversupporter@gmail.com>
2014-04-12 00:41:59 +0200

    Added LoginViewModel and AccountController for handling login requests.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Lisa.Zuma.BlueJay.Web.Models;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lisa.Zuma.BlueJay.Web.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel loginModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "User name or password is not set!");
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+	}
+}
