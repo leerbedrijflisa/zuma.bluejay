92c29e82efab374ecf2b2b7128f6be16359cc982
Robert <azforeversupporter@gmail.com>
2014-04-12 00:33:02 +0200

    Added basic implementation of the Authentication mechanism.

--- src/Web/Lisa.Zuma.BlueJay.Web/Global.asax.cs
+++ src/Web/Lisa.Zuma.BlueJay.Web/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +18,11 @@ namespace Lisa.Zuma.BlueJay.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Set NameIdentifier claim type to validate the AntiForgeryToken with.
+            // When removing the line below, issuing a call to @Html.AntiForgeryToken() will raise an error
+            // because of a missing claim type.
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
         }
     }
 }
