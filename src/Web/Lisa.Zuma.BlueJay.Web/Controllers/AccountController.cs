e3ed9cb22fb5513ed030c503664b17d3a5451c79
Robert <azforeversupporter@gmail.com>
2014-04-13 21:37:05 +0200

    Made use of the newly added method LoginAndGetIdentityAsync.

--- src/Web/Lisa.Zuma.BlueJay.Web/Controllers/AccountController.cs
+++ src/Web/Lisa.Zuma.BlueJay.Web/Controllers/AccountController.cs
@@ -37,25 +37,25 @@ namespace Lisa.Zuma.BlueJay.Web.Controllers
             }
 
             var apiHelper = new WebApiHelper("http://localhost:14689");
-            var loginResult = await apiHelper.LoginAsync(loginModel.Username, loginModel.Password);
-
-            if (loginResult.ContainsKey("error"))
+            var identity = await apiHelper.LoginAndGetIdentityAsync(loginModel.Username, loginModel.Password, DefaultAuthenticationTypes.ApplicationCookie);
+            if (identity == null)
             {
                 ModelState.Clear();
                 ModelState.AddModelError("", "The user name or password is incorrect!");
                 return View();
             }
 
-            var accessToken = loginResult["access_token"];
-            var accessTokenClaim = new Claim("http://leerbedrijflisa.nl/zuma/bluejay/token", accessToken, ClaimValueTypes.String);
-            var claims = await apiHelper.GetClaimsAsync(accessToken);
-            claims.Add(accessTokenClaim);
+            // Find the expire claim that specifies the timestamp on which the access token will
+            // be invalid. Extract the date for use with the AuthenticationManager.SignIn() method,
+            // then delete the expireClaim.
+            var expireClaim = identity.FindFirst("http://leerbedrijflisa.nl/zuma/bluejay/expire");
+            var expireDate = DateTime.Parse(expireClaim.Value).ToUniversalTime();
+            identity.RemoveClaim(expireClaim);
 
-            var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
             AuthenticationManager.SignIn(new AuthenticationProperties
             {
                 IsPersistent = false,
-                ExpiresUtc = DateTime.Parse(loginResult[".expires"]).ToUniversalTime()
+                ExpiresUtc = expireDate
             }, identity);
 
             return Redirect(returnUrl);
