f4a1915920f8fe927a34a05c7fce8f2473791827
Robert <azforeversupporter@gmail.com>
2014-04-13 20:31:38 +0200

    Added WebApiHelper class for making calls to the WebApi. Contains methods for synchronous and a-synchronous calls.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web/Helpers/WebApiHelper.cs
@@ -0,0 +1,70 @@
+﻿using Lisa.Zuma.BlueJay.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Lisa.Zuma.BlueJay.Web.Helpers
+{
+    public sealed class WebApiHelper
+    {
+        public WebApiHelper(Uri rootUri)
+        {
+            this.rootUri = rootUri;
+        }
+
+        public WebApiHelper(string root)
+            : this(new Uri(root))
+        {
+        }
+
+        public Dictionary<string, string> Login(string username, string password)
+        {
+            return LoginAsync(username, password).Result;
+        }
+
+        public async Task<Dictionary<string, string>> LoginAsync(string username, string password)
+        {
+            var client = new RestClient(rootUri.AbsoluteUri);
+            var request = new RestRequest("/token", Method.POST);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded")
+                .AddParameter("grant_type", "password")
+                .AddParameter("username", username)
+                .AddParameter("password", password);
+
+            var response = await client.ExecuteTaskAsync<Dictionary<string,string>>(request);
+            return response.Data;            
+        }
+
+        public List<Claim> GetClaims(string accessToken)
+        {
+            return GetClaimsAsync(accessToken).Result;
+        }
+
+        public async Task<List<Claim>> GetClaimsAsync(string accessToken)
+        {
+            var client = new RestClient(rootUri.AbsoluteUri);
+            var request = new RestRequest("/api/account/userclaims", Method.GET);
+            request.AddHeader("Authorization", string.Format("bearer {0}", accessToken));
+
+            var response = await client.ExecuteTaskAsync<List<UserClaim>>(request);
+            var claims = response.Data
+                .Select(uc =>
+                {
+                    var claim = new Claim(uc.Type, uc.Value, uc.ValueType);
+                    claim.Properties.Concat(uc.Properties);
+
+                    return claim;
+                })
+                .ToList();
+
+            return claims;
+        }
+
+        private Uri rootUri;
+    }
+}
