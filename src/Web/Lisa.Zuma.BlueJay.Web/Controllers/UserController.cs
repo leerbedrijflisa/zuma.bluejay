b57c7ea20b4b9d5aa63b09c3b6bee713d2f605f1
Robert <azforeversupporter@gmail.com>
2014-04-14 22:21:47 +0200

    Added UserController with corresponding views.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Lisa.Zuma.BlueJay.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lisa.Zuma.BlueJay.Web.Controllers
+{
+    public class UserController : BaseController
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(UserViewModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var result = await WebApiHelper.RegisterUserAsync(userModel);
+            if (!result.Success)
+            {
+                ModelState.Clear();
+
+                foreach (var error in result.Errors)
+                {
+                    foreach (var message in error.Value)
+                    {
+                        ModelState.AddModelError(error.Key, message);
+                    }
+                }
+
+                return View();
+            }
+
+            return RedirectToAction("Index");
+        }
+	}
+}
