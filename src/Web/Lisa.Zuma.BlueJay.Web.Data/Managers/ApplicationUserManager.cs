97dae3c368bbc1d6cbd6a5078985a083ea4b6f38
Robert <azforeversupporter@gmail.com>
2014-04-15 11:00:10 +0200

    Added ApplicationUserManager<TUser> because of missing functionality in the default UserManager<TUser> implementation.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web.Data/Managers/ApplicationUserManager.cs
@@ -0,0 +1,31 @@
+﻿using Lisa.Zuma.BlueJay.Web.Data.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lisa.Zuma.BlueJay.Web.Data.Managers
+{
+    public class ApplicationUserManager<TUser> : UserManager<TUser> where TUser : IdentityUser
+    {
+        public ApplicationUserManager(IUserStore<TUser> store)
+            : base(store)
+        {
+            context = ((UserStore<TUser>)base.Store).Context;
+        }
+
+        /// <summary>
+        /// Gets all the users available in the database.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<TUser> GetAll()
+        {
+            return context.Set<TUser>().AsEnumerable();
+        }
+
+        private DbContext context;
+    }
+}
