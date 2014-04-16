9e699911f95ef4d8c1fd6e02f67d84d99465be45
Robert <azforeversupporter@gmail.com>
2014-04-17 00:21:38 +0200

    Added WebApiRoleHelper to make role based requests.

---
+++ src/Web/Lisa.Zuma.BlueJay.Web/Helpers/WebApiRoleHelper.cs
@@ -0,0 +1,50 @@
+﻿using Lisa.Zuma.BlueJay.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Lisa.Zuma.BlueJay.Web.Helpers
+{
+    public class WebApiRoleHelper : WebApiHelper
+    {
+        public WebApiRoleHelper(string rootUri)
+            : base(rootUri)
+        {
+        }
+
+        public List<UserRole> GetRoles()
+        {
+            return GetRolesAsync().Result;
+        }
+
+        public async Task<List<UserRole>> GetRolesAsync()
+        {
+            var request = new RestRequest(roleUri, Method.GET);
+            request.AddParameter(AuthorizationHeader);
+
+            var result = await Client.ExecuteTaskAsync<List<UserRole>>(request);
+            return result.Data;
+        }
+
+        public UserRole AddRole(UserRole role)
+        {
+            return AddRoleAsync(role).Result;
+        }
+
+        public async Task<UserRole> AddRoleAsync(UserRole role)
+        {
+            var request = new RestRequest(roleUri, Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddParameter(AuthorizationHeader)
+                .AddBody(role);
+
+            var result = await Client.ExecuteTaskAsync<UserRole>(request);
+            return result.Data;
+        }
+
+        private const string roleUri = "/api/role";
+    }
+}
