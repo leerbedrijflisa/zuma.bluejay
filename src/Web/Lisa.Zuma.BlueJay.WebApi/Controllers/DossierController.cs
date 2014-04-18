9939692403c7c6b89ed9111ef561dc846a47106b
Robert <azforeversupporter@gmail.com>
2014-04-18 18:43:07 +0200

    Added route attributes to the controller and actions. Added action to create a dossier.

--- src/Web/Lisa.Zuma.BlueJay.WebApi/Controllers/DossierController.cs
+++ src/Web/Lisa.Zuma.BlueJay.WebApi/Controllers/DossierController.cs
@@ -1,4 +1,5 @@
-﻿using Lisa.Zuma.BlueJay.Web.Data;
+﻿using Lisa.Zuma.BlueJay.Models;
+using Lisa.Zuma.BlueJay.Web.Data;
 using Lisa.Zuma.BlueJay.Web.Data.Entities;
 using Lisa.Zuma.BlueJay.Web.Data.Extensions;
 using Lisa.Zuma.BlueJay.WebApi.Helpers;
@@ -8,13 +9,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Lisa.Zuma.BlueJay.WebApi.Controllers
 {
     [Authorize]
+    [RoutePrefix("api/dossier")]
     public class DossierController : BaseApiController
     {
+        [Route("{id}")]
         public IHttpActionResult Get(int id)
         {
             var dossier = default(DossierData);
@@ -28,11 +32,37 @@ namespace Lisa.Zuma.BlueJay.WebApi.Controllers
             return Ok(dossierModel);
         }
 
+        [Route("")]
         public IHttpActionResult Get()
         {
             var dossierModels = Converter.ToDossier(CurrentUser.Dossiers);
 
             return Ok(dossierModels);
+        }
+
+        [Route("{userId}")]
+        [HttpPost]
+        public async Task<IHttpActionResult> Post(string userId, [FromBody] Dossier dossier)
+        {
+            var user = await UserManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var dossierData = new DossierData()
+            {
+                Name = dossier.Name,
+                Watchers = new List<UserData>()
+            };
+            dossierData.Watchers.Add(user);
+
+            UoW.DossierRepository.Insert(dossierData);
+            UoW.Save();
+
+            var result = Converter.ToDossier(dossierData);
+
+            return Created(Request.RequestUri, result);
         }
     }
 }
